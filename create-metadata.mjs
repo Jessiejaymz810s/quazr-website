3f4d72285f8030930887afa3a9d0dc632369093d
jjaybeeze <beck14038@gmail.com>
2026-06-29 18:47:58 -0400

    Update metadata URL to use quazr-website

--- create-metadata.mjs
+++ create-metadata.mjs
@@ -10,10 +10,10 @@ import path from "path";
 import os from "os";
 
 // --- Configuration ---
-const MINT_ADDRESS = "EqzogyGM1RKUdwpNgUeuWLy6L8wiFxGxFQcugkiPsemc";
-const TOKEN_NAME = "Quazr";
-const TOKEN_SYMBOL = "QZSOL";
-const METADATA_URI = "https://raw.githubusercontent.com/Jessiejaymz810s/1sttoken1-14684bf365541cf108d53ec4e32cab3c0d2fa029-1-/main/token-metadata.json";
+const MINT_ADDRESS = "5xNuRxxiGjf4FzpCqLgC65V4FTBeMxFZYerGpydhKboj";
+const TOKEN_NAME = "American Flag Token";
+const TOKEN_SYMBOL = "FLAG";
+const METADATA_URI = "https://raw.githubusercontent.com/Jessiejaymz810s/quazr-website/main/american-flag-metadata.json";
 const RPC_URL = "https://api.mainnet-beta.solana.com";
 
 async function main() {
